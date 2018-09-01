42336711e199807e1426af8d053dc3c50b3df220
ryanr <ryanr@iastate.edu>
2018-08-31 23:48:51 -0500

    WIP: Share a volume for the sql socket with the backend (untested)

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const net = require("net");
 
 let app = express();
 
-app.get("/", (r, re) => re.end("Hello world"));
+app.get("/", (r, re) => {
+    net.connect(process.env.MYSQL_ADDRESS).on("error", err => {
+        re.end(err && err.message);
+    });
+});
 
 app.listen(3000, "0.0.0.0");
--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM node:8.11
 COPY package*.json ./
 RUN npm install --production
 
-COPY . .
+COPY Backend .
+COPY Frontend static
 
 EXPOSE 3000
 
