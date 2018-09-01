14fbe4823cd563e17f69194c0833f823bb4cb26c
JacobDawson98 <dawson@iastate.edu>
2018-09-01 17:28:52 -0500

    Feature: Routes, views, and styles defined
    [https://trello.com/c/jqraYmVt/5-i-can-create-an-account]

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,11 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
+var path = require('path');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const dashboardRouter = require('./routes/dashboard');
+const publicRouter = require('./routes/public');
 
 var app = express();
 
@@ -20,11 +20,11 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(frontendDir, 'public')));
+app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', publicRouter);
+app.use('/dashboard', dashboardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,7 +32,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
--- Backend/routes/dashboard.js
+++ Backend/routes/dashboard.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/');
+router.get('/', (req, res) => {
+    res.render('dashboard');
 });
 
 module.exports = router;
