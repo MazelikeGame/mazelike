674e51e0d2647e0f64b882fd0572313db4df4213
ryanr <ryanr@iastate.edu>
2018-09-01 17:53:43 -0500

    Make the dog movable with the arrow keys.

--- Experiments/game.js
+++ Experiments/game.js
@@ -1,33 +1,33 @@
+import {KeyHandler, KEY_CODES} from "./input.js";
+
 let app = new PIXI.Application({
-    width: 300,
-    height: 300,
     antialias: true
 });
 
 document.body.appendChild(app.view);
 
-app.renderer.backgroundColor = 0xffffff; //0x061639;
-
+// make the game fill the window
 app.renderer.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
 app.renderer.autoResize = true;
 (onresize = () => app.renderer.resize(innerWidth, innerHeight))();
 
+// load the textures
 PIXI.loader
     .add("floor", "DawnLike/Objects/Floor.png")
     .add("dog", "DawnLike/Characters/Dog0.png")
     .load(setup);
 
-const GRASS_WIDTH = 39;
-const GRASS_HEIGHT = 35;
+// the width and height of the grass square
+const GRASS_WIDTH = 45;
+const GRASS_HEIGHT = 41;
 
 function setup() {
+    // get the grass texture from the sprite sheet
     let grassTexture = PIXI.loader.resources.floor.texture;
-    grassTexture.frame = new PIXI.Rectangle(116, 55, GRASS_WIDTH, GRASS_HEIGHT);
+    grassTexture.frame = new PIXI.Rectangle(114, 52, GRASS_WIDTH, GRASS_HEIGHT);
 
-    let dogTexture = PIXI.loader.resources.dog.texture;
-    dogTexture.frame = new PIXI.Rectangle(0, 0, 16, 16);
-
+    // render the ground
     for(let y = 0; y < innerHeight; y += GRASS_HEIGHT) {
         for(let x = 0; x < innerWidth; x += GRASS_WIDTH) {
             let floor = new PIXI.Sprite(grassTexture);
@@ -39,37 +39,48 @@ function setup() {
         }
     }
 
+    let dogTexture = PIXI.loader.resources.dog.texture;
+    dogTexture.frame = new PIXI.Rectangle(0, 0, 16, 16);
+
     let dog = new PIXI.Sprite(dogTexture);
 
-    dog.x = 0;
-    dog.y = 0;
+    // position the dog
+    dog.position.set(app.stage.width / 2, app.stage.height / 2);
+    dog.scale.set(2, 2);
 
-    onmousemove = e => {
-        dog.position.set(e.clientX, e.clientY);
-    };
-
-    onmousewheel = e => {
-        dog.width = Math.min(64, Math.max(16, dog.width + e.deltaY));
-        dog.height = Math.min(64, Math.max(16, dog.height + e.deltaY));
-    };
-
-    dog.anchor.x = 0.5;
-    dog.anchor.y = 0.5;
-
-    let timer;
-    onclick = () => {
-        clearInterval(timer);
-        timer = setInterval(() => {
-            dog.rotation += 0.1;
-
-            if(dog.rotation >= 2 * Math.PI) {
-                dog.rotation = 0;
-                clearInterval(timer);
-            }
-        }, 10);
-    };
+    // bind the dogs velocity values (vx and vy) to the arrow keys
+    new VelocityMod(dog, "vx", KEY_CODES.RIGHT_ARROW, KEY_CODES.LEFT_ARROW);
+    new VelocityMod(dog, "vy", KEY_CODES.DOWN_ARROW, KEY_CODES.UP_ARROW);
 
     app.stage.addChild(dog);
 
-    app.renderer.render(app.stage);
+    // move the dog based on its velocity every 60th of a second
+    app.ticker.add(() => {
+        dog.x += dog.vx;
+        dog.y += dog.vy;
+    });
+}
+
+/**
+ * VelocityMod takes an object, a direction, an up and a down key.
+ * VelocityMod sets object.direction to 1 when up keys is pressed and to -1
+ * when the down key is pressed.
+ */
+class VelocityMod extends KeyHandler {
+    constructor(object, direction, upKey, downKey) {
+        super(upKey, downKey);
+        this.obj = object;
+        this.dir = direction;
+        this.upKey = upKey;
+
+        this.obj[this.dir] = 0;
+    }
+
+    onDown(key) {
+        this.obj[this.dir] = key == this.upKey ? 1 : -1;
+    }
+
+    onUp() {
+        this.obj[this.dir] = 0;
+    }
 }
