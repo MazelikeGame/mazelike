ec4656983fb336ee6601121472192f0a80143622
csurfus <csurfus@iastate.edu>
2018-09-02 21:53:23 -0500

    HTTP serves local files

--- Experiments/Clayton/PixiExperiment/app.js
+++ Experiments/Clayton/PixiExperiment/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+/**
+ * Simple web server to serve the game files.
+ */
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile('index.html');
 });
 
 app.listen(3000, function () {
