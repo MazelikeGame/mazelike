4c41522eb060892cedf1f295797e608b0c590ec1
JacobDawson98 <dawson@iastate.edu>
2018-09-08 15:50:02 -0500

    WIP: More routes defined, working in deprecations
    [private]

--- Backend/app.js
+++ Backend/app.js
@@ -56,6 +56,72 @@ var sessionChecker = (req, res, next) => {
     }
 };
 
+// route for Home-Page
+app.get('/', sessionChecker, (req, res) => {
+    res.redirect('/login');
+});
+
+// route for user signup
+app.route('/signup')
+    .get(sessionChecker, (req, res) => {
+        res.sendFile(frontendDir + '/views/signup.pug');
+    })
+    .post((req, res) => {
+        User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+            .then((user) => {
+                req.session.user = user.dataValues;
+                res.redirect('/dashboard');
+            })
+            .catch((error) => {
+                res.redirect('/signup');
+            });
+    });
+
+// route for user Login
+app.route('/login')
+    .get(sessionChecker, (req, res) => {
+        res.sendFile( + '/views/login.pug');
+    })
+    .post((req, res) => {
+        var username = req.body.username,
+            password = req.body.password;
+
+        User.findOne({ where: { username: username } }).then(function(user) {
+            if (!user) {
+                res.redirect('/login');
+            } else if (!user.validPassword(password)) {
+                res.redirect('/login');
+            } else {
+                req.session.user = user.dataValues;
+                res.redirect('/dashboard');
+            }
+        });
+    });
+
+// route for user's dashboard
+app.get('/dashboard', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.sendFile(frontendDir + '/views/dashboard.pug');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+// route for user logout
+app.get('/logout', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.clearCookie('user_sid');
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
