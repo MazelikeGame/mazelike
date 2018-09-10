39b443ed260a5d9ada6253cbc3645d1d2424050f
JacobDawson98 <dawson@iastate.edu>
2018-09-10 13:23:51 -0500

    Feature: Dog can now move with arrow keys
    [Speedy quick]

--- Experiments/jacobd_pixi_experiments/public/game.js
+++ Experiments/jacobd_pixi_experiments/public/game.js
@@ -1,3 +1,4 @@
+import { keyboard } from './keyboard.js';
 /* global PIXI */
 
 // Init app
@@ -19,12 +20,73 @@ onresize();
 app.renderer.resize(window.innerWidth, window.innerHeight);
 app.renderer.backgroundColor = 0x6dcff6;
 
+//Capture the keyboard arrow keys
+
+let left = keyboard(37),
+  up = keyboard(38),
+  right = keyboard(39),
+  down = keyboard(40);
+
+let dog; let state;
 function setup() {
   let dogTexture = PIXI.loader.resources.dog.texture;
   dogTexture.frame = new PIXI.Rectangle(0, 0, 16, 16);
-  let dog = new PIXI.Sprite(dogTexture);
+  dog = new PIXI.Sprite(dogTexture);
   dog.x = 150;
   dog.y = 150;
+  dog.vx = 0;
+  dog.vy = 0;
+
+  //Left arrow key `press` method
+  left.press = () => {
+    //Change the dog's velocity when the key is pressed
+    dog.vx = -5;
+    dog.vy = 0;
+  };
+
+  //Left arrow key `release` method
+  left.release = () => {
+    //If the left arrow has been released, and the right arrow isn't down,
+    //and the dog isn't moving vertically:
+    //Stop the dog
+    if (!right.isDown && dog.vy === 0) {
+      dog.vx = 0;
+    }
+  };
+
+  //Up
+  up.press = () => {
+    dog.vy = -5;
+    dog.vx = 0;
+  };
+  up.release = () => {
+    if (!down.isDown && dog.vx === 0) {
+      dog.vy = 0;
+    }
+  };
+
+  //Right
+  right.press = () => {
+    dog.vx = 5;
+    dog.vy = 0;
+  };
+  right.release = () => {
+    if (!left.isDown && dog.vy === 0) {
+      dog.vx = 0;
+    }
+  };
+
+  //Down
+  down.press = () => {
+    dog.vy = 5;
+    dog.vx = 0;
+  };
+  down.release = () => {
+    if (!up.isDown && dog.vx === 0) {
+      dog.vy = 0;
+    }
+  };
+  state = play;
 
   document.addEventListener('keydown', (event) => {
     let bootTexture = PIXI.loader.resources.boot.texture;
@@ -45,8 +107,13 @@ function setup() {
 }
 
 function gameLoop(delta, sprite) {
-  console.log(delta);
-  sprite.x += 1 + delta;
+  sprite.x += delta;
+  state(delta);
+}
+
+function play(delta) {
+  dog.y += dog.vy;
+  dog.x += dog.vx;
 }
 
 // Add textures
