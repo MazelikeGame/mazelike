f5c49dcb9251c3f87dd50bd8c2df1cfc378c34c1
csurfus <csurfus@iastate.edu>
2018-09-09 23:49:42 -0500

    Added more tests

--- Experiments/Clayton/JasmineExperiment/README.md
+++ Experiments/Clayton/JasmineExperiment/README.md
@@ -1,6 +1,7 @@
 # Jasmine Experiment
 ## Resources
-I researched the [Jasmine documentation](https://jasmine.github.io/index.html).
+I researched the [Jasmine documentation](https://jasmine.github.io/index.html). A pretty good tutorial
+can be found on the [Jasmine website](https://jasmine.github.io/2.1/introduction).
 
 The goal was to learn the basics and syntax of Jasmine.
 
--- Experiments/Clayton/JasmineExperiment/spec/PlayerSpec.js
+++ Experiments/Clayton/JasmineExperiment/spec/PlayerSpec.js
@@ -1,15 +1,30 @@
 const Player = require('../player.js');
 
 
-
 describe("Player Suite", function() {
-    var Bob = new Player("Bob", 0, 0);
+  var Bob = new Player("Bob", 0, 0);
 
-    it("1.0 Player's name should be defined", function() {
-      expect(Bob.Name).toBeDefined();
-    });
-
-    it("1.1 Player's name should be Bob", function() {
-      expect(Bob.Name).toBe("Bob");
-    });
+  it("1.0 Player's name should be defined", function() {
+    expect(Bob.Name).toBeDefined();
   });
+
+  it("1.1 Player's name should be Bob", function() {
+    expect(Bob.Name).toBe("Bob");
+  });
+
+  it("1.1 Player's name should be not be bob", function() {
+    expect(Bob.Name).not.toBe("bob");
+  });
+});
+
+describe("Move Test", function() {
+  var Bob = new Player("Bob", 0, 0);
+
+  it("2.0 Player's x value should be equal to 0.", function() {
+    expect(Bob.x).toEqual(0)
+  });
+
+  it("2.0 Player's x value should be equal to 0.", function() {
+    expect(Bob.x).not.toEqual(0)
+  });
+});
