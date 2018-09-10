5e76daef6ebe4ad244402bbacf2b4c5cbbe034af
JacobDawson98 <dawson@iastate.edu>
2018-09-10 13:10:08 -0500

    Enhancement: Boots do not stop now
    [When another boot has been shot the other still continues to move]

--- Experiments/jacobd_pixi_experiments/public/game.js
+++ Experiments/jacobd_pixi_experiments/public/game.js
@@ -19,21 +19,19 @@ onresize();
 app.renderer.resize(window.innerWidth, window.innerHeight);
 app.renderer.backgroundColor = 0x6dcff6;
 
-let dog;
-let boot;
 function setup() {
   let dogTexture = PIXI.loader.resources.dog.texture;
   dogTexture.frame = new PIXI.Rectangle(0, 0, 16, 16);
-  dog = new PIXI.Sprite(dogTexture);
+  let dog = new PIXI.Sprite(dogTexture);
   dog.x = 150;
   dog.y = 150;
 
   document.addEventListener('keydown', (event) => {
     let bootTexture = PIXI.loader.resources.boot.texture;
+    bootTexture.frame = new PIXI.Rectangle(0, 0, 16, 16);
+    let boot = new PIXI.Sprite(bootTexture);
     switch(event.keyCode) {
     case 32:
-      bootTexture.frame = new PIXI.Rectangle(0, 0, 16, 16);
-      boot = new PIXI.Sprite(bootTexture);
       app.ticker.add((delta) => {
         gameLoop(delta, boot);
       });
