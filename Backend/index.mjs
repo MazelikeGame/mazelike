bf7a6cbce42bc9808ea0d74e997e490e7bdfb70f
Ryan Ray <ryan314r@gmail.com>
2018-09-10 15:19:50 -0500

    Move everything out of experiments.

---
+++ Backend/index.mjs
@@ -0,0 +1,29 @@
+import express from "express";
+import http from "http";
+import socketio from "socket.io";
+
+let app = express();
+let server = http.Server(app);
+let io = socketio(server);
+
+app.use(express.static("frontend"));
+
+let nextId = 0;
+
+io.on("connection", (client) => {
+  const id = ++nextId;
+
+  client.once("ready", () => {
+    client.emit("id", id);
+  });
+
+  client.on("position", (pos) => {
+    io.emit("set", pos);
+  });
+
+  client.on("disconnect", () => {
+    io.emit("remove", id);
+  });
+});
+
+server.listen(3000, "localhost");
