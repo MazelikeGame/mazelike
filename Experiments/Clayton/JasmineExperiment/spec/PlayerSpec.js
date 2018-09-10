95ff6046fcad1e3407eaf45e8f988d68044a8ab8
csurfus <csurfus@iastate.edu>
2018-09-09 21:50:37 -0500

    Fixed Jasmine syntax.

--- Experiments/Clayton/JasmineExperiment/spec/PlayerSpec.js
+++ Experiments/Clayton/JasmineExperiment/spec/PlayerSpec.js
@@ -1,10 +1,15 @@
 const Player = require('../player.js');
 
-var test = new Player("Hello", 0, 0);
-test.Move();
-console.log(test.x);
-describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-      expect(test.Name).toBe("Helflo");
+
+
+describe("Player Suite", function() {
+    var Bob = new Player("Bob", 0, 0);
+
+    it("1.0 Player's name should be defined", function() {
+      expect(Bob.Name).toBeDefined();
+    });
+
+    it("1.1 Player's name should be Bob", function() {
+      expect(Bob.Name).toBe("Bob");
     });
   });
