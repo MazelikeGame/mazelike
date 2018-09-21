a68635bb1fbcdfc5940ed362ebdd10663bfe3a68
JacobDawson98 <dawson@iastate.edu>
2018-09-21 13:02:19 -0500

    Test: Create routes tests
    [https://trello.com/c/jqraYmVt/5-as-a-user-i-can-create-an-account]

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -2,6 +2,7 @@
 var request = require('request');
 var base_url = 'http://localhost:3000/';
 var login_url = 'http://localhost:3000/account/login';
+var create_url = 'http://localhost:3000/account/create'
 
 
 describe("An example test", () => {
@@ -10,10 +11,29 @@ describe("An example test", () => {
   });
 });
 
+describe('Create route tests', () => {
+  it('Can visit /create', () => {
+    request.get(create_url, function(err, response, body) {
+      expect(response.statusCode.toBe(200));
+      expect(response.url.toBe(create_url));
+    });
+  });
+
+  it('Can create an account', () => {
+    request.post({
+      url: create_url,
+      form: { username: 'test', password: 'test', email: 'test@test.com' }
+    }, function(err, response, body) {
+      expect(response.url).toBe(login_url);
+    });
+  });
+});
+
 describe('Login route tests', () => {
   it('Can visit login', () => {
     request.get(login_url, function(err, response, body) {
       expect(response.statusCode).toBe(200);
+      expect(response.url.toBe(login_url));
     });
   });
 
