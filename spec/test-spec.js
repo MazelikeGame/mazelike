3d9ea9f0ac4ac64ebc4f20bec3721480a1b58646
JacobDawson98 <dawson@iastate.edu>
2018-09-21 12:12:23 -0500

    Test: Visiting login and logging in with default creds
    [https://trello.com/c/jqraYmVt/5-as-a-user-i-can-create-an-account]

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -1,7 +1,28 @@
 /* global describe it */
+var request = require('request');
+var base_url = 'http://localhost:3000/';
+var login_url = 'http://localhost:3000/account/login';
+
 
 describe("An example test", () => {
   it("Passes", () => {
     // throw new Error("Fail");
+  });
+});
+
+describe('Login route tests', () => {
+  it('Can visit login', () => {
+    request.get(login_url, function(err, response, body) {
+      expect(response.statusCode).toBe(200);
+    });
+  });
+
+  it('Can login with default credentials', () => {
+    request.post({
+      url: login_url,
+      form: { username: 'test', password: 'password' }
+    }, function(err, response, body) {
+      expect(response.url).toBe(base_url);
+    });
   });
 });
