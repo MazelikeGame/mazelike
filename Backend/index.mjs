bcfa1579a89592ca63c6248536ce2dd8b00a2411
Ryan Ray <ryan314r@gmail.com>
2018-09-21 23:30:51 -0500

    Create prototype game lobby

--- Backend/index.mjs
+++ Backend/index.mjs
@@ -1,17 +1,17 @@
 import express from "express";
 import http from "http";
 import socketio from "socket.io";
-import gameRouter from "./routes/game.mjs";
+import {gameRouter, joinRoute} from "./routes/game.mjs";
 import exphbs from "express-handlebars";
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 
 let app = express();
 let server = http.Server(app);
 let io = socketio(server);
 
 app.use(express.static("Frontend"));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
 
 //Handlebars
 app.engine('handlebars', exphbs());
@@ -20,6 +20,7 @@ app.set('views', 'Frontend/views');
 
 //Routes
 app.use("/game", gameRouter);
+app.get("/j/:id", joinRoute);
 
 let nextId = 0;
 
--- Backend/routes/game.mjs
+++ Backend/routes/game.mjs
@@ -1,15 +1,112 @@
 import express from "express";
+import crypto from "crypto";
 
-const gameRouter = express.Router();
+export let gameRouter = express.Router();
+
+let lobbies = new Map();
 
 gameRouter.get("/lobby/:id", (req, res) => {
+  if(!lobbies.has(req.params.id)) {
+    res.render("lobby", {
+      invalid: true,
+      user: req.query.user
+    });
+
+    return;
+  }
+
+  let lobby = lobbies.get(req.params.id);
+
   res.render("lobby", {
-    id: req.param.id,
-    isHost: req.query.user === "host",
+    id: req.params.id,
+    isHost: req.query.user === lobby.host,
     user: req.query.user,
-    players: (req.query.person || [])
-      .map((k, j) => ({ name: k, isHost: k == "host", id: j }))
+    players: lobby.players,
+    host: lobby.host,
+    origin: `http://${req.headers.host}`
   });
 });
 
-export default gameRouter;
+// /j/:id (from the root)
+export const joinRoute = (req, res) => {
+  if(!req.query.user) {
+    res.redirect("/login");
+    return;
+  }
+
+  let lobby = lobbies.get(req.params.id);
+
+  lobby.players.push({
+    id: req.query.user,
+    name: req.query.user
+  });
+
+  res.redirect(`/game/lobby/${req.params.id}`);
+};
+
+gameRouter.get("/new", (req, res) => {
+  if(!req.query.user) {
+    res.redirect("/login");
+    return;
+  }
+
+  crypto.randomBytes(9, (_, b) => {
+    let id = b.toString("base64")
+      .replace(/\//g, "-")
+      .replace(/\+/g, "_");
+
+    lobbies.set(id, {
+      host: req.query.user,
+      players: [{
+        id: req.query.user,
+        name: req.query.user,
+        isHost: true
+      }]
+    });
+
+    res.redirect(`/game/lobby/${id}`);
+  });
+});
+
+gameRouter.get("/lobby/:id/delete", (req, res) => {
+  if(!req.query.user) {
+    res.redirect("/login");
+    return;
+  }
+
+  let lobby = lobbies.get(req.params.id);
+
+  if(lobby) {
+    if(lobby.host === req.query.user) {
+      lobbies.delete(req.params.id);
+    } else {
+      res.end("Only the host can delete this lobby.");
+      return;
+    }
+  }
+
+  res.end("Lobby deleted");
+});
+
+gameRouter.get("/lobby/:id/drop/:player", (req, res) => {
+  if(!req.query.user) {
+    res.redirect("/login");
+    return;
+  }
+
+  let lobby = lobbies.get(req.params.id);
+
+  if(lobby) {
+    let idx = lobby.players.findIndex((player) => {
+      return player.id === req.params.player;
+    });
+
+    if(idx !== -1) {
+      lobby.players.splice(idx, 1);
+    }
+
+    res.end("Player removed");
+  }
+
+  res.end("No such lobby");
+});
