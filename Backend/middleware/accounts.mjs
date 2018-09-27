4f29b835107c93a19e25b3130fa4c01bfbbcd574
Ryan Ray <ryan314r@gmail.com>
2018-09-26 22:47:21 -0500

    Update account middleware to work with old user model.

--- Backend/middleware/accounts.mjs
+++ Backend/middleware/accounts.mjs
@@ -1,4 +1,7 @@
-import User from "../models/user";
+import userFn from "../models/user";
+import sql from "../sequelize";
+
+let User = userFn(sql);
 
 // Get the user model for the current user
 export default async function userMiddleware(req, res, next) {
