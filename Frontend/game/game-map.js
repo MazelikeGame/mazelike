9fbd47721f300d5b04cb73d859eb190a52ba02b1
Ryan Ray <ryan314r@gmail.com>
2018-10-07 22:21:30 -0500

    WIP: Add maze generation

--- Frontend/game/game-map.js
+++ Frontend/game/game-map.js
@@ -1,17 +1,21 @@
 /* eslint-disable complexity,no-extra-parens,no-mixed-operators */
-import {dbgLog} from "./debug.js";
-let debugGen = dbgLog("generator");
+// import {dbgLog} from "./debug.js";
+// let debugGen = dbgLog("generator");
 
-const BLOCK_SIZE = 48;
-const SIZE = 100;
+const BLOCK_SIZE = 16;
+let SIZE = 20;
 
 /**
  * Map a 2d coordinate to a 1d coordinate
  * @param {*} x 
  * @param {*} y 
  */
-const d2 = (x, y) => {
-  return y * SIZE + x;
+const d21 = (x, y, size = SIZE) => {
+  return y * size + x;
+};
+
+const d12 = (i, size = SIZE) => {
+  return [i % size, Math.floor(i / size)];
 };
 
 /**
@@ -26,49 +30,81 @@ export default class GameMap {
    * Geneate a game map
    * @param {Array} generators The generators for creating sections of the map
    */
-  static generate(generators) {
+  static generate() {
     let map = new GameMap();
-    debugGen.log("Generating map");
-    let start = Date.now();
-    
-    for(let y = 0; y < SIZE; y++) {
-      for(let x = 0; x < SIZE; x++) {
-        // check if this spot is taken
-        if(map.map[d2(x, y)]) {
-          continue;
-        }
+    let unvisited = new Set();
+    let corridors = new Map();
+    let stack = [];
 
-        // try to find a generator that fits
-        for(;;) {
-          let generator = generators[Math.floor(Math.random() * generators.length)];
-
-          if((generator.rarity || 1) < Math.random()) {
-            continue;
-          }
-
-          let fits = true;
-          
-          // Check all the blocks that this generator wants to use to ensure they are empty
-          for(let yf = y; yf < generator.height + y && fits; ++yf) {
-            for(let xf = x; xf < generator.width + x && fits; ++xf) {
-              fits = yf < SIZE &&
-                xf < SIZE &&
-                !map.map[d2(xf, yf)];
-            }
-          }
-
-          if(!fits) {
-            continue;
-          }
-
-          debugGen.log(`Using generator ${generator.name} ${generator.width}x${generator.height} at (${x}, ${y})`);
-          generator.generate(map, x, y);
-          break;
-        }
-      }
+    for(let i = 0; i < SIZE ** 2; ++i) {
+      unvisited.add(i);
     }
 
-    debugGen.log(`Map generated ${Date.now() - start}ms`);
+    let startingPoint = Math.floor(Math.random() * unvisited.size);
+    stack.push(startingPoint);
+
+    while(stack.length) {
+      let current = stack[stack.length - 1];
+      let [x, y] = d12(current);
+      
+      // find all unvisited neighbours
+      let unvisitedNeighbours = [];
+
+      if(unvisited.has(d21(x + 1, y)) && x + 1 < SIZE) {
+        unvisitedNeighbours.push(d21(x + 1, y));
+      }
+
+      if(unvisited.has(d21(x, y + 1)) && y + 1 < SIZE) {
+        unvisitedNeighbours.push(d21(x, y + 1));
+      }
+
+      if(unvisited.has(d21(x - 1, y)) && x - 1 >= 0) {
+        unvisitedNeighbours.push(d21(x - 1, y));
+      }
+
+      if(unvisited.has(d21(x, y - 1)) && y - 1 >= 0) {
+        unvisitedNeighbours.push(d21(x, y - 1));
+      }
+
+      // all of our neighbours have been visited go back to the previous node
+      if(!unvisitedNeighbours.length) {
+        stack.pop();
+        continue;
+      }
+
+      // visit a neighbouring cell
+      let toCell = unvisitedNeighbours[Math.floor(Math.random() * unvisitedNeighbours.length)];
+      unvisited.delete(toCell);
+
+      // create the edges for the corridors
+      if(!corridors.has(toCell)) {
+        corridors.set(toCell, new Set());
+      }
+
+      if(!corridors.has(current)) {
+        corridors.set(current, new Set());
+      }
+
+      corridors.get(toCell).add(current);
+      corridors.get(current).add(toCell);
+
+      stack.push(toCell);
+    }
+
+    SIZE *= 2;
+
+    for(let i = 0; i < (SIZE / 2) ** 2; ++i) {
+      let [x, y] = d12(i, SIZE / 2);
+      x *= 2;
+      y *= 2;
+      map.map[d21(x, y)] = "0-0-box-small";
+
+      /* eslint-disable max-len */
+      map.map[d21(x + 1, y)] = (corridors.get(i) || new Set()).has(d21(x / 2 + 1, y / 2, SIZE / 2)) ? "0-1-box-small" : "";
+      map.map[d21(x, y + 1)] = (corridors.get(i) || new Set()).has(d21(x / 2, y / 2 + 1, SIZE / 2)) ? "0-1-box-small" : "";
+      /* eslint-enable max-len */
+    }
+
     return map;
   }
 
@@ -82,15 +118,11 @@ export default class GameMap {
           y: y * BLOCK_SIZE,
           width: BLOCK_SIZE,
           height: BLOCK_SIZE,
-          type: this.map[d2(x, y)]
+          type: this.map[d21(x, y)]
         });
       }
     }
 
     return out;
-  }
-
-  set(x, y, type) {
-    this.map[d2(x, y)] = type;
   }
 }
