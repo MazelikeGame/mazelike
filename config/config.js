78da14785dcab393a148461548ad23e14fc99778
JacobDawson98 <dawson@iastate.edu>
2018-10-08 09:29:22 -0500

    Feature: Add config file for sequelize-cli

---
+++ config/config.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+};
