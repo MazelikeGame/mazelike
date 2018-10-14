c72dccdf35bbf39afc61a7b114d03a5bfe164be6
Katie <kmr@iastate.edu>
2018-10-14 17:10:38 -0500

    dog monster on map

--- Frontend/game/Monster.js
+++ Frontend/game/Monster.js
@@ -3,6 +3,9 @@
 /** @module Monster */
 
 import GameMap from "/shared/game-map.mjs";
+
+const BLOCK_SIZE = 48;
+
 export default class Monster {
   
   constructor(name_in, hp_in, damage_in) {
@@ -10,22 +13,15 @@ export default class Monster {
     this.hp = hp_in;
     this.damage = damage_in;
     this.targetAquired = false; // if monster knows where a player is
-    this.x = 0;
-    this.y = 0;
+    this.x = 5;
+    this.y = 5;
     //PCx: -1; // -1 if not seen yet
     //PCy: -1;
 
     this.sprite = new PIXI.Sprite(PIXI.loader.resources.dog.textures["0-0"]);
-    this.sprite.position.set((this.x * GameMap.BLOCK_SIZE) - GameMap.xMin, (this.y * GameMap.BLOCK_SIZE) - GameMap.yMin);
-    this.sprite.width = GameMap.BLOCK_SIZE;
-    this.sprite.height = GameMap.BLOCK_SIZE;
-
-    // this.sprite = new PIXI.Text("m", this._textStyle);
-    // this.sprite.setText("m");
-    // this.x = 3;
-    // this.y = 3;
-    // this.sprite.position.set((this.x * GameMap.BLOCK_SIZE) - GameMap.xMin, (this.y * GameMap.BLOCK_SIZE) - GameMap.yMin);
-    // this.sprite.style.fill = 0xFFFFFF;
+    this.sprite.position.set(this.x * BLOCK_SIZE, this.y * BLOCK_SIZE);
+    this.sprite.width = BLOCK_SIZE;
+    this.sprite.height = BLOCK_SIZE;
 
     this.create();
   }
@@ -37,35 +33,36 @@ export default class Monster {
   wander() { // if PC not seen yet OR last seen PC location has been explored
     // need to check for collisions/ make sure its a floor piece, WIP
     
-    // var random = Math.floor(Math.random() * 4); 
-    // if(random === 0)
-    //   this.x += 1;
-    // else if(random === 1)
-    //   this.x -= 1;
-    // else if(random === 2)
-    //   this.y += 1;
-    // else if(random === 3)
-    //   this.y -= 1;
-    // setInterval(this.wander(), 1000);
+    var random = Math.floor(Math.random() * 4); 
+    if(random === 0)
+      this.x += 1;
+    else if(random === 1)
+      this.x -= 1;
+    else if(random === 2)
+      this.y += 1;
+    else if(random === 3)
+      this.y -= 1;
+    this.sprite.position.set(this.x * BLOCK_SIZE, this.y * BLOCK_SIZE);
+    //setInterval(this.wander(), 1000);
   }
   move() {
     if(!this.targetAquired) {
       this.wander();
     } else {
-      //move strategically, to be implemented later when PC is on map
+      //move strategically, to be implemented later when PC is on map WIP
     }
   }
   attack(target) {
     var msg = " attacked ";
     console.log(this.name + msg + target);
-    // implement damage later when PC is on map
+    // implement damage later when PC is on map WIP
   }
   create() {
     var msg = " created.";
     console.log(this.name + msg);
-    // place on map
+    this.wander();
   }
   die() {
-    //clearInterval(alive);
+    //clearInterval(alive); WIP
   }
 }
