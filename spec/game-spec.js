6fda74cc14ccc9ef30197b2554c16c8e32a51881
csurfus <csurfus@iastate.edu>
2018-10-14 21:17:27 -0500

    Setup Babel (ES6 Support) with Jasmine.

---
+++ spec/game-spec.js
@@ -0,0 +1,9 @@
+const chai = require("chai");
+const game = require("../Frontend/game/input.js");
+
+describe('Keyboard Inputs', () => {
+  it('Can press D', (done) => {
+    chai.assert(true, "Testing!");
+    done();
+  });
+});
