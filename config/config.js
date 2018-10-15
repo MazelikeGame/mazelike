11a0f5b75d60bfb831fd868a6404dde538781d63
JacobDawson98 <dawson@iastate.edu>
2018-10-15 14:20:39 -0500

    Feature: Add dialect to config

--- config/config.js
+++ config/config.js
@@ -6,21 +6,21 @@ module.exports = {
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: process.env.DB_STORAGE ? "sqlite" : 'mysql'
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: process.env.DB_STORAGE ? "sqlite" : 'mysql'
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: process.env.DB_STORAGE ? "sqlite" : 'mysql'
   }
 };
 
