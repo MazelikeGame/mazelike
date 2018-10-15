1420e6a288076cdba7b966000e603bdfef6d0b53
JacobDawson98 <dawson@iastate.edu>
2018-10-15 15:28:50 -0500

    Feature: Migrate changes made in user model

--- config/config.js
+++ config/config.js
@@ -23,33 +23,3 @@ module.exports = {
     dialect: process.env.DB_STORAGE ? "sqlite" : 'mysql'
   }
 };
-
-// {
-//   "development": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_development",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "test": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_test",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "production": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_production",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   }
-// }
-=======
-    dialect: process.env.DB_STORAGE ? "sqlite" : 'mysql'
-  }
-};
-
->>>>>>> dd8bf0c101703a52951f572015a33df2d487acde
