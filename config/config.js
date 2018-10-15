d585cc7fc1b58ab649d1ce17486888ace69d672d
Ryan Ray <ryan314r@gmail.com>
2018-10-15 15:44:49 -0500

    Correct sqlite config

--- config/config.js
+++ config/config.js
@@ -6,20 +6,23 @@ module.exports = {
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_STORAGE ? "sqlite" : 'mysql'
+    dialect: process.env.DB_STORAGE ? "sqlite" : 'mysql',
+    storage: process.env.DB_STORAGE
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_STORAGE ? "sqlite" : 'mysql'
+    dialect: process.env.DB_STORAGE ? "sqlite" : 'mysql',
+    storage: process.env.DB_STORAGE
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_STORAGE ? "sqlite" : 'mysql'
+    dialect: process.env.DB_STORAGE ? "sqlite" : 'mysql',
+    storage: process.env.DB_STORAGE
   }
 };
