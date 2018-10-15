24f0ba6aa98c109703063de5c7c6b5f52d0bbd53
JacobDawson98 <dawson@iastate.edu>
2018-10-14 23:10:49 -0500

    Feature: Add encrypt functions to new User model
    [https://trello.com/c/7KPmUPQn/52-as-a-developer-i-can-generate-migrations-and-run-migrations-after-modifying-a-table]

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+const bycrypt = require('bcrypt');
+
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
@@ -12,5 +14,21 @@ module.exports = (sequelize, DataTypes) => {
   User.associate = function(models) {
     // associations can be defined here
   };
+
+  User.encryptPassword = function(password, callback) {
+    bcrypt.hash(password, 10, function(err, hash) {
+      callback(err, hash);
+    });
+  };
+
+  User.comparePassword = function(password, realPassword, callback) {
+    bcrypt.compare(password, realPassword, function(err, result) {
+      callback(err, result);
+    });
+  };
+
+
+  // add methods here as user.method = function() {};
+  // do not use an => if you want to use this in the function
   return User;
 };
