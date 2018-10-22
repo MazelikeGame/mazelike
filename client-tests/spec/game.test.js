d9518a69389388ac5bf32e644f232bd9181ad37a
csurfus <csurfus@iastate.edu>
2018-10-22 00:36:48 -0500

    Updated

---
+++ client-tests/spec/game.test.js
@@ -0,0 +1,8 @@
+import Monster from '../Frontend/game/Monster.js';
+
+describe('Calculator', function() {
+	it('should add numbers', () => {
+    var temp = new Monster("Hei", 5, 5);
+    expect(window).toBe(true);
+  });
+});
