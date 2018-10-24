7573276e581549ede4e5dbe01c903a4da4ec0101
csurfus <csurfus@iastate.edu>
2018-10-24 14:35:48 -0500

    Added addPlayer method

--- Frontend/game/player-list.js
+++ Frontend/game/player-list.js
@@ -1,33 +1,41 @@
 /*global PIXI*/
 export default class PlayerList {
+
   constructor(listOfPlayers) {
     this.listOfPlayers = listOfPlayers;
+    this.graphics = new PIXI.Graphics();
   }
 
   render() {
+    this.listOfPlayers.forEach((player) => {
+      this.addPlayer(player, 100);
+    });
+
+    return this.graphics;
+  }
+
+  addPlayer(playerName, playerHP) {  
     this._textStyle = new PIXI.TextStyle({
       fill: "#fff",
       fontSize: 17
     });
-
-    var graphics = new PIXI.Graphics();
     
     //BACKGROUND
-    graphics.beginFill(0xFFFFFF);
-    graphics.fillAlpha = 0.15;
-    graphics.drawRect(0, 0, 250, 200);
-    graphics.position.set(10, 10);
-    graphics.endFill();
+    this.graphics.beginFill(0xFFFFFF);
+    this.graphics.fillAlpha = 0.15;
+    this.graphics.drawRect(0, 0, 250, 200);
+    this.graphics.position.set(10, 10);
+    this.graphics.endFill();
 
     //TITLE
     let title = new PIXI.Text("Players", this._textStyle);
     title.position.set(10, 5);
-    graphics.addChild(title);
+    this.graphics.addChild(title);
 
     //PLAYER NAME
-    let player = new PIXI.Text("CSurfus", this._textStyle);
+    let player = new PIXI.Text(playerName, this._textStyle);
     player.position.set(10, 30);
-    graphics.addChild(player);
+    this.graphics.addChild(player);
 
     var health2 = new PIXI.Graphics();
     health2.beginFill(0xFF0000);
@@ -35,16 +43,16 @@ export default class PlayerList {
     health2.position.set(140, 37);
     health2.endFill();
 
-    graphics.addChild(health2);
+    this.graphics.addChild(health2);
 
     //HP BOX
     var health = new PIXI.Graphics();
     health.beginFill(0x7CFC00);
-    health.drawRect(0, 0, 0, 10);
+    health.drawRect(0, 0, playerHP, 10);
     health.position.set(140, 37);
     health.endFill();
 
-    graphics.addChild(health);
+    this.graphics.addChild(health);
 
     //HP TEXT
     this._hpTextStyle = new PIXI.TextStyle({
@@ -52,10 +60,8 @@ export default class PlayerList {
       fontSize: 9
     });
 
-    let HP_TEXT = new PIXI.Text("100", this._hpTextStyle);
-    HP_TEXT.position.set(180, 37);
-    graphics.addChild(HP_TEXT);
-
-    return graphics;
+    let HP_TEXT = new PIXI.Text(playerHP, this._hpTextStyle);
+    HP_TEXT.position.set(190, 37);
+    this.graphics.addChild(HP_TEXT);
   }
 }
