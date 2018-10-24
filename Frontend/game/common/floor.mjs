a2fc76972f7575e92864a3d30d817fb1e93215d2
Katie <kmr@iastate.edu>
2018-10-24 17:46:30 -0500

    tweaked monster-room ratio and updated the version

--- Frontend/game/common/floor.mjs
+++ Frontend/game/common/floor.mjs
@@ -12,8 +12,8 @@ class Floor {
    */
   constructor(gameId, floorIdx) {
     this.id = `${gameId}-${floorIdx}`;
-    // below: ratio of Monsters to Rooms. ex: 0.3333 puts monsters in a third of the rooms.
-    this.monsterRatio = 0.3333;
+    // below: ratio of Monsters to Rooms. ex: 0.4 puts monsters in a fourth of the rooms.
+    this.monsterRatio = 0.4;
     // Initialize players, monsters and the game map in loadBrowser, loadNode,  and generate methods
   }
 
