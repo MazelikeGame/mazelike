111b63cbb25961372649b9a9a6a706450721c2ad
JacobDawson98 <dawson@iastate.edu>
2018-10-24 10:44:39 -0500

    Enhancement: Add primary key fields and doc to game and player models
    [https://trello.com/c/dSAdhcCa/44-as-a-developer-there-is-a-player-class]

--- Backend/models/game.mjs
+++ Backend/models/game.mjs
@@ -5,10 +5,19 @@ import Player from './player';
 let Game = sql.define('games', {
   gameId: {
     type: Sequelize.STRING,
+    primaryKey: true,
     allowNull: false
   },
 });
 
 Game.hasMany(Player, { as: 'Players' });
 
+/**
+ * The game model. Has many to many relationship with the Player model.
+ * Instances of Game will get the accessors `getPlayers` and `setPlayers`.
+ * @param {Integer} gameId - The unique game id. The same name as the json file
+ * in public
+ * @param {Player[]} Players - Foreign key, many to many relationship
+ * with the Player model.
+ */
 export default Game;
--- Backend/models/player.mjs
+++ Backend/models/player.mjs
@@ -3,6 +3,10 @@ import sql from '../sequelize';
 import User from './user';
 
 let Player = sql.define('players', {
+  uuid: {
+    type: Sequelize.UUID,
+    primaryKey: true
+  },
   numPlayer: {
     type: Sequelize.INTEGER,
     allowNull: false
@@ -19,4 +23,12 @@ let Player = sql.define('players', {
 
 Player.belongsTo(User, { as: 'Username' });
 
+/**
+ * The player model. Has one to many relationship with the User model.
+ * @param {UUID} uuid - The primary key.
+ * @param {Integer} numPlayer - Every game can have up to 4 players.
+ * This refers to which of the 4 players this player is
+ * @param {String} items - The list of all items the player has.
+ * @param {Integer} floor - The floor number the player is on
+ */
 export default Player;
