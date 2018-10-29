96a7f55bfba15f5786ef82fb0dc27fb907f6715e
Ryan Ray <ryan314r@gmail.com>
2018-10-28 21:56:51 -0500

    Switch to single docker-compose file

--- scripts/test-stopper.js
+++ scripts/test-stopper.js
@@ -3,14 +3,14 @@ const fs = require("fs");
 
 function dockerComposeDown() {
   try {
-    child_process.execSync("docker-compose -f docker-compose.yml down");
+    child_process.execSync("docker-compose down");
   } catch(err) {
     // Do nothing
   }
 }
 
 // Display the logs
-let logArgs = ["-f", "docker-compose.yml", "logs", "-f"];
+let logArgs = ["logs", "-f"];
 
 if(process.argv.length > 2) {
   logArgs.push(process.argv[2]);
@@ -24,14 +24,13 @@ process.on("SIGINT", dockerComposeDown);
 
 // wait for result to be written to runner-result
 let timer;
-fs.watch("runner-result/runner-result", () => {
+fs.watch("runner-result", () => {
   clearTimeout(timer);
   timer = setTimeout(() => {
-    let status = +fs.readFileSync("runner-result/runner-result", "utf8");
+    let status = +fs.readFileSync("runner-result", "utf8");
 
     dockerComposeDown();
-    fs.unlinkSync("runner-result/runner-result");
-    fs.rmdirSync("runner-result");
+    fs.unlinkSync("runner-result");
 
     process.exit(status);
   }, 500);
