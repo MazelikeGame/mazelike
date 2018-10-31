04250597b6ce11f61e1a723806a77a80bf7bf3f0
JacobDawson98 <dawson@iastate.edu>
2018-10-31 15:38:55 -0500

    Debugging: Attempt to migrate user correctly

--- migrations/20181031195953-add-hasMany-with-player-to-user.js
+++ migrations/20181031195953-add-hasMany-with-player-to-user.js
@@ -2,25 +2,55 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn(
-      'players', // name of target model
-      'username', // name of the key we are adding
-      {
-        type: Sequelize.UUID,
-        references: {
-          model: 'users', // name of source model
-          key: 'id' // key in source model that is being referenced
-        },
-        onDelete: 'SET NULL',
-        onUpdate: 'CASCADE'
-      }
-    );
+    return [
+      queryInterface.createTable(
+        'players',
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          createdAt: {
+            type: Sequelize.DATE
+          },
+          updatedAt: {
+            type: Sequelize.DATE
+          },
+          spriteName: {
+            type: Sequelize.STRING,
+            allowNull: false
+          },
+          numFloor: {
+            type: Sequelize.STRING,
+            defaultValue: 0,
+            allowNull: false
+          }
+        }
+      ),
+      queryInterface.addColumn(
+        'players', // name of target model
+        'username', // name of the key we are adding
+        {
+          type: Sequelize.UUID,
+          references: {
+            model: 'users', // name of source model
+            key: 'id' // key in source model that is being referenced
+          },
+          onDelete: 'SET NULL',
+          onUpdate: 'CASCADE'
+        }
+      ),
+    ];
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn(
-      'players', // name of target model
-      'username' // name of the key to remove
-    );
+    return [
+      queryInterface.removeColumn(
+        'players', // name of target model
+        'username' // name of the key to remove
+      ),
+      queryInterface.dropTable('players')
+    ];
   }
 };
