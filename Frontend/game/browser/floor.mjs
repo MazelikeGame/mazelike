456482c4d2819306f5003dfa590c2a9e085f5f96
Ryan Ray <ryan314r@gmail.com>
2018-10-31 15:13:33 -0500

    Get monsters working again

--- Frontend/game/browser/floor.mjs
+++ Frontend/game/browser/floor.mjs
@@ -14,6 +14,7 @@ export default class Floor extends FloorCommon {
     this._viewportY = 0;
 
     this.monsters = [];
+    this.monsterSprites = new PIXI.Container();
   }
 
   /**
@@ -40,7 +41,6 @@ export default class Floor extends FloorCommon {
    * @param {Floor} floor The floor to add monsters to
    */
   generateMonsters() {
-    this.monsterSprites = new PIXI.Container();
     this.monsters = [];
     let random = 0;
     for(let i = 0; i < this.map.rooms.length * this.monsterRatio; i++) { 
@@ -151,6 +151,7 @@ export default class Floor extends FloorCommon {
     this._diffState("id", this.monsters, state.monsters, (raw) => {
       let monster = new Monster(raw.name, raw.hp, 10, this, raw.id, raw.type);
       monster.setCoodinates(raw.x, raw.y);
+      monster.createSprite();
       return monster;
     });
   }
