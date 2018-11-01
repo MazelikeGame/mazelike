e343603472cfc618db1be8a62562ecf10b174f7f
JacobDawson98 <dawson@iastate.edu>
2018-11-01 15:14:59 -0500

    Bug Fix: Create lobbies table migration. That way backend tests can pass

---
+++ migrations/20181031194733-create-lobbies-table.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+      'lobbies',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        createdAt: {
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          type: Sequelize.DATE
+        },
+        lobbyId: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        isHost: {
+          type: Sequelize.BOOLEAN,
+          defaultValue: false
+        },
+        playerId: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        secret: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+      }
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    queryInterface.dropTable('lobbies');
+  }
+};
