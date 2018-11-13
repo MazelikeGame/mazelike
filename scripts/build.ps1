fc58bdac4fbd847150c52a833a664fe73d9936e0
Ryan Ray <ryan314r@gmail.com>
2018-11-13 12:30:56 -0600

    Add more control to build script

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,6 +1,21 @@
+param(
+  [switch]$devel,
+  [switch]$latest
+)
+
+$TAG = "ryan3r/mazelike:$(cat VERSION)"
+
+if($devel) {
+  $TAG = "mazelike"
+}
+
 if("$(docker version -f "{{.Server.Os}}")" -eq "windows") {
-  docker build --build-arg BASE=stefanscherer/node-windows:8.11 --build-arg DATA="C:\data" -t ryan3r/mazelike:$(cat VERSION) .
+  docker build --build-arg BASE=stefanscherer/node-windows:8.11 --build-arg DATA="C:\data" -t $TAG .
 }
 else {
-  docker build -t ryan3r/mazelike:$(cat VERSION) .
+  docker build -t $TAG .
+}
+
+if($latest) {
+  docker tag $TAG ryan3r/mazelike:latest
 }
