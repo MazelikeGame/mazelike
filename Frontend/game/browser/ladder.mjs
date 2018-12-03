4d6c5929abc9db1bd954fd2a1a91ebded8e9930a
csurfus <csurfus@iastate.edu>
2018-12-02 20:31:07 -0600

    Added comments

--- Frontend/game/browser/ladder.mjs
+++ Frontend/game/browser/ladder.mjs
@@ -1,6 +1,7 @@
 import LadderCommon from "../common/ladder.mjs";
 
 /*global PIXI*/
+/** @module Item */
 export default class Ladder extends LadderCommon {
   constructor() {
     super();
@@ -8,12 +9,22 @@ export default class Ladder extends LadderCommon {
     this.sprite.position.set(this.x, this.y);
   }
 
+  /**
+   * Sets the ladder's x and y values.
+   * @param x the x value we want to set x
+   * @param y the y value we want to set y
+   */
   setPosition(x, y) {
     this.x = x;
     this.y = y;
     this.sprite.position.set(this.x, this.y);
   }
 
+  /**
+   * Updates the sprite position
+   * @param viewX the view x
+   * @param viewY the view y
+   */
   update(viewX, viewY) {
     this.sprite.position.set(this.x - viewX, this.y - viewY);
   }
